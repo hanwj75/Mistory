c25a436e1633c4e09d01e8cca4a86d8d53b25fba
hanwj75 <113535534+hanwj75@users.noreply.github.com>
2022-11-11 19:20:02 +0900

    Update middleware.js

--- services/middleware.js
+++ services/middleware.js
@@ -1,36 +1,21 @@
-import jwt from "jsonwebtoken";
+import { verifyToken } from "./jwtToken.js";
 
-// import { verifyToken } from "./jwtToken.js";
+export let tokenMiddleware = (req, res, next) => {
+  const userId = req.body.userId;
+  const authorizationToken = req.headers.authorization.split(" ")[1];
+  const decodedToken = verifyToken;
 
-// export const authorizationToken = (req, res, next) => {
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-//   if (token == null) return res.jsonstatus(200), next(), verifyToken;
-// };
+  if (authorizationToken !== userId) {
+    decodedToken;
+    console.log(userId);
+    console.log(authorizationToken);
+  }
 
-// export const verifyToken = (req, res, next) => {
-//   const secretKey = process.env.SECRET_KEY;
-
-//   jwt.verify(req.headers.authorization, secretKey);
-//   console.log(req.headers.authorization);
-
-//   return next();
-// };
-
-export let verifyToken = (req, res, next) => {
-  const secretKey = process.env.SECRET_KEY;
-  const token = req.headers.authorization;
-  const userId = req.headers.userId;
-  jwt.verify(token, secretKey, (err, result) => {
-    console.log(token);
-    console.log(result);
-
-    if (token !== userId) {
-      return res.status(400).json({ message: "토큰오류", err });
-    }
-    if (token === userId) {
-      res.status(200).json({ message: "토큰확인" });
-      return next();
-    }
-  });
+  if (authorizationToken === userId) {
+    console.log(decoded);
+    res.status(200).json({ message: "토큰 확인!", decoded });
+    return next();
+  }
 };
+
+// 브라우저 헤더에 토큰이 있으면 토큰을 디코딩해서 유저의 id와 같다면 다음으로 넘어감

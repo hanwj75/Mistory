aa9d264eca74adab9cf7538bfbd1a02e500df254
hanwj75 <wjws9608@gmail.com>
2022-11-16 16:20:55 +0900

    '미들웨어수정'

--- services/middleware.js
+++ services/middleware.js
@@ -7,15 +7,15 @@ export let tokenMiddleware = (req, res, next) => {
 
   if (!encodedToken) {
     res.status(400).json({message:'토큰발급이 필요함'})
-  }
+  return console.log('토큰 없음')}
   if (!decodedToken) {
-    res.status(400).json({ message: "토큰 없음" });
-    return console.log("토큰없음");
+    res.status(400).json({ message: "토큰 검증실패" });
+    return console.log("토큰 검증실패");
   }else{
     return next()
   }
 
   
 };
-
+//토큰이 없다면 400 토큰이 틀리면 400 토큰이 맞다면 다음으로 넘어감
 // 브라우저 헤더에 토큰이 있으면 토큰을 디코딩해서 유저의 id와 같다면 다음으로 넘어감1313qweqe111git g

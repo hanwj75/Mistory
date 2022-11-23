1e5d57b44972f3dc0f08e8990b9af6e8dc203ac1
hanwj75 <wjws9608@gmail.com>
2022-11-23 15:15:25 +0900

    '미들웨어수정중2'

--- services/middleware.js
+++ services/middleware.js
@@ -4,22 +4,18 @@ export let tokenMiddleware = (req, res, next) => {
   const encodedToken = req.headers.authorization.split(" ")[1];
   const decodedToken = verifyToken(req);
 
-  if (!encodedToken) {
+  if (encodedToken===null) {
     res.status(400).json({ message: "토큰 발급이 필요함" });
-    return console.log("토큰 없음");
-  } else {
-    res.status(400).json({ message: "토큰 검증이 필요함" });
-  }
-  if (encodedToken) {
-    decodedToken;
-  }
-  if (decodedToken === false) {
-    res.status(400).json({ message: "토큰 검증 실패!" });
-    return;
-  } else {
-    res.status(200).json({ message: "토큰 검증 완료!" });
-  }
-  return next();
+    return false}
+  else{
+    return next()
+    }
+    req.headers.decodedToken=decodedToken
+    next()
+    if(!decodedToken){
+      return false
+      
+  ;}return next()
 };
 //토큰이 없다면 400 토큰이 틀리면 400 토큰이 맞다면 다음으로 넘어감
 // 브라우저 헤더에 토큰이 있으면 토큰을 디코딩해서 유저의 id와 같다면 다음으로 넘어감

6f8035a0a8af6c3fad265f61d4d41dd938d3d4f9
hanwj75 <wjws9608@gmail.com>
2022-11-28 09:38:06 +0900

    '미들웨어수정완료'

--- services/middleware.js
+++ services/middleware.js
@@ -1,15 +1,35 @@
-import { db } from "../app.js";
+
 import { verifyToken } from "./jwtToken.js";
 
-export let tokenMiddleware = (req, res, next) => {
-  const encodedToken = req.headers.authorization.split(" ")[1];
-  const decodedToken = verifyToken(req);
+// export let tokenMiddleware = (req, res, next) => {
+//   const encodedToken = req.headers.authorization.split(" ")[1];
+//   const decodedToken = verifyToken(req);
 
-  if (encodedToken===null) {
+//   if (encodedToken===null) {
+//     res.status(400).json({ message: "토큰 발급이 필요함" });
+//   return false}
+
+// if(!decodedToken){
+//   res.status(400).json({message:"토큰이 다릅니다"})
+// return
+// }else{
+//   return next()
+ 
+// }
+// };
+//토큰이 없다면 400 토큰이 틀리면 400 토큰이 맞다면 다음으로 넘어감
+// 브라우저 헤더에 토큰이 있으면 토큰을 디코딩해서 유저의 id와 같다면 다음으로 넘어감
+export let tokenMiddleware = (req, res, next) => {
+ 
+  const decodedToken = verifyToken(req);
+const userObjectId=req.params.id
+
+  if (decodedToken===null) {
     res.status(400).json({ message: "토큰 발급이 필요함" });
   return false}
 
-if(!decodedToken){
+if(decodedToken!==userObjectId){
+  console.log(userObjectId)
   res.status(400).json({message:"토큰이 다릅니다"})
 return
 }else{
@@ -17,5 +37,3 @@ return
  
 }
 };
-//토큰이 없다면 400 토큰이 틀리면 400 토큰이 맞다면 다음으로 넘어감
-// 브라우저 헤더에 토큰이 있으면 토큰을 디코딩해서 유저의 id와 같다면 다음으로 넘어감
